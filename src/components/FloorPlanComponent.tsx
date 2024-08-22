65d6dd58dec3a44e4c8c872dc98ceee8f60294d0
Grant Finn <fosterffinn@gmail.com>
2024-08-22 14:03:46 -0400

    Added new pages - all new rooms Added hover functionality Added images for some rooms Removed old styling

--- src/components/FloorPlanComponent.tsx
+++ src/components/FloorPlanComponent.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../styles/FloorPlan.module.css';
+import modernLivingRoom from '../images/grey-living-room-ideas-1.jpg';
+import midcenturyModernKitchen from '../images/midcenturyModernKitchen.jpg'
+
 
 const FloorPlanComponent: React.FC = () => {
   return (
     <div className={styles.floorPlan}>
       <div className={styles.room} id={styles.livingRoom}>
-        <Link to="/about">Living Room </Link>
+        <Link to="/about">
+            <img src={modernLivingRoom} alt='Modern Living Room' />
+        </Link>
       </div>
       <div className={styles.room} id={styles.workshop}>
-        <Link to="/projects">Kitchen </Link>
+        <Link to="/projects"> 
+            <img src={midcenturyModernKitchen} alt='Midcentury Modern Kitchen'/>
+        </Link>
       </div>
       <div className={styles.room} id={styles.office}>
         <Link to="/office">Office </Link>
