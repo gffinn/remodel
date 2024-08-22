f806f9e48ec63742ba188775c77bc31ed90400fd
Grant Finn <fosterffinn@gmail.com>
2024-08-22 14:22:38 -0400

    Added new logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 // src/components/Header.tsx
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from '../styles/Header.module.css'; // Correct relative path
-
+import styles from '../styles/Header.module.css'; 
+import logo from '../images/logo.jpg'
 const Header: React.FC = () => {
   return (
     <header className={styles.headerContainer}>
-      <div className={styles.logo}>Renovation</div>
+      <div className={styles.logo}><img src={logo} alt="logo" /></div>
       <nav className={styles.nav}>
         <Link to="/" className={styles.navLink}>Home</Link>
         <Link to="/about" className={styles.navLink}>About</Link>
