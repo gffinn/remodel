358a6c2fadb5af40e463c000bac7e4932fc66813
Grant Finn <fosterffinn@gmail.com>
2024-08-26 21:14:55 -0400

    refactor header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,22 @@
-// src/components/Header.tsx
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from '../styles/Header.module.css'; 
-import logo from '../images/logo.jpg'
-const Header: React.FC = () => {
-  return (
-    <header className={styles.headerContainer}>
-      <div className={styles.logo}><img src={logo} alt="logo" /></div>
-      <nav className={styles.nav}>
-        <Link to="/" className={styles.navLink}>Home</Link>
-        <Link to="/about" className={styles.navLink}>About</Link>
-        <Link to="/projects" className={styles.navLink}>Projects</Link>
-        <Link to="/contact" className={styles.navLink}>Contact</Link>
-      </nav>
-      <button className={styles.button}>Get in Touch</button>
-    </header>
-  );
+import styles from '../styles/Header.module.css';
+import logo from '../images/logo.jpg';
+
+const Header = () => {
+    return (
+        <>
+            <div className={styles.header}>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
+            </div>
+            <div className={styles.logoDiv}>
+                <img src={logo} alt="Logo" />
+            </div>
+        </>
+    );
 };
 
 export default Header;
