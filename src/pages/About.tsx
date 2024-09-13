297c886dc18dcd5b8fcb477e456d15c9598ffe52
Grant Finn <fosterffinn@gmail.com>
2024-09-12 20:31:19 -0400

    Update AboutPage

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,12 +1,13 @@
 // src/pages/About.tsx
 import React from "react";
 import Header from "../components/Header";
+import ProjectVision from "../components/ProjectVision";
 
 const About: React.FC = () => {
   return (
     <>
       <Header />
-      <div>About</div>
+      <ProjectVision/>
     </>
   );
 };
